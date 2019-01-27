86390673d58703e29d8e8139ca3f33c50416005d
Glenn <glennpws@hotmail.co.uk>
2019-01-27 04:31:33 +0000

    Title screen!

---
+++ GGJ19/Assets/Scripts/ButtonToHub.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class ButtonToHub : MonoBehaviour
+{
+    public Button transition;
+
+    void Start()
+    {
+        transition.onClick.AddListener(TaskOnClick);
+    }
+
+    void TaskOnClick()
+    {
+        Scene sceneToLoad = SceneManager.GetSceneByName("Hub");
+        SceneManager.LoadScene("Hub");
+    }
+}
